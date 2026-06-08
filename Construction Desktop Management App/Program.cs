90eb344cde56b5f8b7ac5250bb6a058bb079fd11
Parah <122792967+ParahDime@users.noreply.github.com>
2026-06-08 21:55:15 +0100

    Created layout for desktop application, basic file structure

--- Construction Desktop Management App/Program.cs
+++ Construction Desktop Management App/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// add npgsql from download
+using System;
+
+namespace ConstructionManagement
+{
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
